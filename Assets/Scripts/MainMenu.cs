f77e8a6b993c8046ba949080c5674f076dec8e64
noamSeb <noam.sebahoun@gmail.com>
2025-01-09 21:53:22 +0100

    :sparkles: Settings section in main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,12 +4,25 @@ using UnityEngine.SceneManagement;
 using Application = UnityEngine.Device.Application;
 using NaughtyAttributes;
 using TMPro;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Game Data")]
     [SerializeField] GameData gameData;
     [SerializeField, Range(2,8)] int _playerNumber = 2;
     [SerializeField] TMP_Text _playerNumberText;
+    
+    [Header("SOUND")]
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] Slider sliderVolume;
+
+    private void Reset()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        Slider sliderVolume = GetComponent<Slider>();
+        audioSource.volume = sliderVolume.value;
+    }
 
     private void Update()
     {
@@ -31,6 +44,9 @@ public class MainMenu : MonoBehaviour
     [Button]
     public void AddPlayer()
     {
+        if (_playerNumber == 8)
+            return;
+        
         _playerNumber++;
     }
     
@@ -38,11 +54,7 @@ public class MainMenu : MonoBehaviour
     public void RemovePlayer()
     {
         if (_playerNumber == 2)
-        {
-            _playerNumber= 8;
             return;
-        }
-            
         
         _playerNumber--;
     }
@@ -50,5 +62,11 @@ public class MainMenu : MonoBehaviour
     {
         gameData.PlayerNumber = _playerNumber;
         Debug.Log("Player count set to: " + gameData.PlayerNumber);
+    }
+    
+    public void ChangeVolume()
+    {
+        Debug.Log("Volume set to: " + sliderVolume.value);
+        audioSource.volume = sliderVolume.value;
     }
 }
